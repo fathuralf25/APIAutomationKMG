570af4166e53dab23fc7fbf99c59cd1be94d0811
fathuralf25 <fathurrahmanalfarizi25@gmail.com>
2026-08-10 11:08:17 +0700

    delete test merge file

--- test_merge.py
+++
@@ -1,9 +0,0 @@
-from docx import Document
-import copy
-doc = Document('collections/TEMPLATE_Defect Report.docx')
-template_tbl = doc.tables[0]
-doc.add_page_break()
-new_tbl = copy.deepcopy(template_tbl._element)
-doc._body._element.append(new_tbl)
-doc.save('test_merge.docx')
-print("Done")
